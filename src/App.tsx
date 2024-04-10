830735bd6312b52c7f24a2a0777f6fa9a5e097c3
masonkulikowski <masonskulikowski@gmail.com>
2024-04-10 10:47:38 -0400

    removing excess

--- src/App.tsx
+++ src/App.tsx
@@ -34,16 +34,12 @@ function App() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
     window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
   }
-  // function navigateHome() {
-  //  window.location.href = '/';
-  //}
+
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
-  //function navigateHome() {
-  //  window.location.href = '/';
-  //}
+
   return (
     <div className="App">
       <header className="App-header">
