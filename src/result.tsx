980ce1b6b83cbc62229dead398c9676e3ff00e82
MelloFlare <dhirpatel91@gmail.com>
2024-05-12 19:07:15 -0400

    added everything into the box

--- src/result.tsx
+++ src/result.tsx
@@ -28,12 +28,10 @@ function Results(){
     }
 
     
-
     return(
         
         <div className='App'>
-            <header className='App-header'>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '40px', backgroundColor: '#4B6D7A', height:'auto' }}>
+            <header className='App-header2'>
                     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',color:"#000",marginBottom: '20px', fontSize: '40px', fontWeight: 'bold', height: '100%' }}>
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', color: "#000", marginBottom: '20px', fontSize: '40px', fontWeight: 'bold', height: '100%' }}>
                             {!generatedResponse ? (
@@ -41,26 +39,28 @@ function Results(){
                                      <h1 style={{ marginBottom: '20px', color: '#000', fontSize: '40px', fontWeight: 'bold' }}>Loading.....</h1>
                             </div>
                             ) : (
-                            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                                <h1 style={{ marginBottom: '20px', color: '#000', fontSize: '40px', fontWeight: 'bold' }}>AI Suggestion</h1>
-                                {generatedResponse.split(".").map((sentence, index) => (
-                                <div key={index} style={{ color: '#fff', fontSize: '20px', alignItems: 'center', width:'1000px', wordWrap:'break-word'}}>{sentence}</div>
-                                ))}
-                                <div>
-                                    <textarea
-                                    rows={2}
-                                    cols={20}
-                                     onChange={updateEmail}/>
-                                     <br />
-                                     <p>{email}</p>
-                                     <button type="button" onClick={() => send} className='Detailed-button'>Send Email</button>
-                                     </div>
-                            </div>
+                                <><div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '40px', backgroundColor: '#4B6D7A', height: 'auto'}}>
+                                    <p style={{ marginBottom: '20px', color: '#000', fontSize: '40px', fontWeight: 'bold' }}>AI Suggestion:</p>
+                                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                                            {generatedResponse.split(".").map((sentence, index) => (
+                                                <div key={index} style={{ color: '#fff', fontSize: '20px', alignItems: 'center', width: '1000px', wordWrap: 'break-word' }}>{sentence}<p></p></div>
+                                            ))}
+                                            <div style={{ color: '#000', fontSize: '20px', alignItems: 'center', width: '1000px', wordWrap: 'break-word' }}>
+                                                <p>Send result</p>
+                                                <textarea
+                                                    rows={2}
+                                                    cols={20}
+                                                    onChange={updateEmail} />
+                                                <br />
+                                                <p>{email}</p>
+                                                <button type="button" onClick={() => send} className='Detailed-button'>Send Email</button>
+                                            </div>
+                                        </div>
+                                    </div></>
                             )
                             }
                         </div>
                     </div>
-                </div>
                 
             </header>
         </div>
